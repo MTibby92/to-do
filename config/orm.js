f1fcd82dd6ebec883fc1c2ff77fc8811d611ea5b
MTibby92 <MTibby92@gmail.com>
2016-10-31 15:31:34 -0500

    wrote orm queries, haven't tested yet

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,23 @@
 var conn = require('./connection')
 
 var orm = {
-	selectAll: function() {
-
+	selectAll: function(table, callback) {
+		conn.query('SELECT * FROM ??', [table], function(err, data) {
+			if (err) throw err;
+			callback(data)
+		})
 	},
-	insertOne: function() {
-
+	insertOne: function(table, name, boolean, callback) {
+		conn.query('INSERT INTO ?? (todo_name, completed) VALUES (?, ?)', [table, name, boolean], function(err, data) {
+			if (err) throw err;
+			callback(data)
+		})
 	},
-	updateOne: function() {
-		
+	updateOne: function(table, id, column, boolean, callback) {
+		conn.query('UPDATE ?? SET ?? = ? WHERE id = ?', [table, column, boolean, id], function(err, data) {
+			if (err) throw err;
+			callback(data)
+		})
 	}
 }
 
