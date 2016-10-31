429e1ad0ede1e33ea7b87b30a48d3dbaf25d8e66
MTibby92 <MTibby92@gmail.com>
2016-10-31 15:01:50 -0500

    built database and setup mysql connection

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO todo (todo_name, completed) VALUES ('Program Assignment', FALSE);
+INSERT INTO todo (todo_name, completed) VALUES ('Put Away Dishes', FALSE);
+INSERT INTO todo (todo_name, completed) VALUES ('Take out Trash', FALSE);
+INSERT INTO todo (todo_name, completed) VALUES ('Do Laundry', FALSE);
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var methodOverride = require('method-override')
 var bodyParser = require('body-parser')
 var path = require('path')
 var exphbs = require('express-handlebars')
+var mysql = require('mysql')
+var pass = require('./secret-key')
 
 
 // Sets up the Express App
@@ -18,9 +20,30 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+
 // Sets up Handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+
+var conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : pass,
+  database : ''
+})
+
+conn.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+    return
+  }
+
+  console.log('connected as id ' + conn.threadId)
+
+}
 
 
 // Starts the server to begin listening
